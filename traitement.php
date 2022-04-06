8eac922f31158615b506e27b5fa2ef3cea881c48
delphine <delphin38@live.fr>
2022-04-06 16:10:42 +0200

    first commit

---
+++ traitement.php
@@ -0,0 +1,11 @@
+<?php
+
+// $Nom = htmlspecialchars($_POST["namename"]);
+// $Email = htmlspecialchars($_POST["nameemail"]);
+// $Message = htmlspecialchars($_POST["namemessage"]);
+
+// echo 'Bonjour' . $Nom;
+// echo 'email' . $Email;
+// echo 'message' . $Message;
+
+var_dump($_POST);
