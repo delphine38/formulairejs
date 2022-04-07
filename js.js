93d384e9af67c1e70ba380e164bab48b07dc749f
delphine <delphin38@live.fr>
2022-04-07 08:55:16 +0200

    fonctionnal field log

--- js.js
+++ js.js
@@ -1,5 +1,13 @@
 const btn = document.querySelector('button');
 const idFirstName = document.getElementById("idFirstName");
+const idLastName = document.getElementById("idLastName");
+const idMail = document.getElementById("idMail");
+const idPhone = document.getElementById("idPhone");
+const post = document.getElementById("post");
+const alternance = document.getElementById("alternance");
+const stage = document.getElementById("stage");
+const idMessage = document.getElementById("idMessage");
+
 const formulaireObjetDelphine = {}
 // const chocolat = document.getElementById('chocolat');
 
@@ -13,6 +21,29 @@ const formulaireObjetDelphine = {}
 btn.addEventListener('click', myFonction);
 function myFonction(event){
     event.preventDefault();
-    formulaireObjetDelphine.idFirstName = idFirstName.value
-    console.log(formulaireObjetDelphine);
+    if (
+        (formulaireObjetDelphine.idFirstName = idFirstName.value) &&
+        (formulaireObjetDelphine.idLastName = idLastName.value) &&
+        (formulaireObjetDelphine.idMail = idMail.value) &&
+        (formulaireObjetDelphine.idPhone = idPhone.value) &&
+        (
+            (formulaireObjetDelphine.post = post.value) ||
+            (formulaireObjetDelphine.alternance = alternance.value) ||
+            (formulaireObjetDelphine.alternance = alternance.value) 
+        ) &&
+        (formulaireObjetDelphine.idMessage = idMessage.value)
+    ){
+        console.log(formulaireObjetDelphine);
+    }
+    
+    // formulaireObjetDelphine.idFirstName = idFirstName.value;
+    // formulaireObjetDelphine.idLastName = idLastName.value;
+    // formulaireObjetDelphine.idMail = idMail.value;
+    // formulaireObjetDelphine.idPhone = idPhone.value;
+    // formulaireObjetDelphine.post = post.value;
+    // formulaireObjetDelphine.alternance = alternance.value;
+    // formulaireObjetDelphine.alternance = stage.value;
+    // formulaireObjetDelphine.idMessage = idMessage.value;
+
+    // console.log(formulaireObjetDelphine);
 }
